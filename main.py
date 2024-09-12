98420036e6d28563c5ab00714f0780ee3a175fb9
Nati Lintzer <nlinzer@gmail.com>
2024-09-13 00:46:11 +0300

    update the project

--- main.py
+++ main.py
@@ -3,27 +3,13 @@ from service.citys_service import (extract_location,
 from api.weather_api import get_cities_weather, get_cities_details, get_city_details
 from repository.json_repository import create_json_file, read_json
 from toolz import get_in
+from service.files_service import create_files_if_not_exist
 
 CITIES_WEATHER = read_json("assets/all_cities_weather.json")
 
 
-
 def main():
-    # cities_details = get_cities_details(TARGETS)
-    # cities_weather = get_cities_weather(TARGETS)
-    # create_json_file(cities_details, "all_cities_details")
-    # create_json_file(cities_weather, "all_cities_weather")
-    isreal_details = get_city_details("tel%aviv")
-    create_json_file(isreal_details, "tel-aviv detail")
-
-
-
-
-
-
-
-
-
+    create_files_if_not_exist()
 
 
 if __name__ == '__main__':
--- service/files_service.py
+++ service/files_service.py
@@ -1,6 +1,6 @@
 import os
 
-from api.weather_api import get_cities_details, get_cities_weather
+from api.weather_api import get_cities_details, get_cities_weather, get_city_details
 from repository.json_repository import read_json, create_json_file
 
 TARGETS = read_json("assets/targets.json")
@@ -15,4 +15,5 @@ def create_files_if_not_exist():
         create_json_file(cities_weather, "all_cities_weather")
 
     if not os.path.isfile("assets/tel-aviv detail.json"):
-
+        isreal_details = get_city_details("tel%aviv")
+        create_json_file(isreal_details, "tel-aviv detail")
